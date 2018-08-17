895d0349cd0bf56040688d5ffcd741fea5c52356
Eric <huynh.eric91@gmail.com>
2018-08-17 13:07:09 -0700

    change2

--- Bootstrap.py
+++ Bootstrap.py
@@ -18,7 +18,7 @@ for _ in range(50):
 # Compute and plot ECDF from original data
 x, y = ecdf(rainfall)
 _ = plt.plot(x, y, marker='.')
-#hi
+
 # Make margins and label axes
 plt.margins(0.02)
 _ = plt.xlabel('yearly rainfall (mm)')
@@ -30,3 +30,33 @@ plt.show()
 #get a bootstrap measurement of statistic func
 def bootstrap_replicate_1d(data, func):
     return func(np.random.choice(data, size=len(data)))
+
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data,func)
+
+    return bs_replicates
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x,bs_y,deg=1)
+
+    return bs_slope_reps, bs_intercept_reps
